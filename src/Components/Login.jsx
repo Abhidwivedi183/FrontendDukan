aed7a3c3c82c8f287b360d9330fe3224298e4a56
Abhidwivedi183 <abhishekdwivedi28062003@gmail.com>
2025-08-08 13:58:05 +0530

    render pe aae gaye

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -25,7 +25,7 @@ const setsubmit = async (e)=>{
   return (
     <div style={{margin:"30px",backgroundColor:"Red",height:"450px",width:"400px", display:"flex", flexDirection:"column", gap:"10px", justifyItems:"space-between",justifyContent:"center",alignItems:"center" ,position:"absolute",top:"120px",left:"505px" , borderRadius:"5%"}}>
       <input type  = "text" placeholder = "Name" name  = "name" style  = {{height:"40px",width:"80%"}}  value ={data.name} onChange={change} />
-      <input type  = "password" placeholder='Price' name = "price" style  = {{height:"40px",width:"80%"}} value ={data.price} onChange={change}/>
+      <input type  = "number" placeholder='Price' name = "price" style  = {{height:"40px",width:"80%"}} value ={data.price} onChange={change}/>
       <input type = "text" placeholder='Enter id' name = "image" style  = {{height:"40px",width:"80%"}} value ={data.image} onChange={change}/>
       <button onClick={setsubmit} style  = {{marginTop:"20px",textAlign:"center",borderRadius:"10px", backgroundColor:"Green" ,fontSize:"28px",cursor:"pointer"}}>Submit</button>
     </div>
